2d2f9bc0055a7a9f251e6a667814d012a8730d28
Wilco Fiers <wilco.fiers@deque.com>
2018-08-19 13:45:31 +0200

    test: wait for load

--- test/integration/full/css-orientation-lock/mocha.js
+++ test/integration/full/css-orientation-lock/mocha.js
@@ -44,211 +44,198 @@
 					function(process, global) {
 						'use strict';
 
-						global.mocha = {
-							setup: function() {}
+						/* eslint no-unused-vars: off */
+						/* eslint-env commonjs */
+
+						/**
+						 * Shim process.stdout.
+						 */
+
+						process.stdout = require('browser-stdout')({ level: false });
+
+						var Mocha = require('./lib/mocha');
+
+						/**
+						 * Create a Mocha instance.
+						 *
+						 * @return {undefined}
+						 */
+
+						var mocha = new Mocha({ reporter: 'html' });
+
+						/**
+						 * Save timer references to avoid Sinon interfering (see GH-237).
+						 */
+
+						var Date = global.Date;
+						var setTimeout = global.setTimeout;
+						var setInterval = global.setInterval;
+						var clearTimeout = global.clearTimeout;
+						var clearInterval = global.clearInterval;
+
+						var uncaughtExceptionHandlers = [];
+
+						var originalOnerrorHandler = global.onerror;
+
+						/**
+						 * Remove uncaughtException listener.
+						 * Revert to original onerror handler if previously defined.
+						 */
+
+						process.removeListener = function(e, fn) {
+							if (e === 'uncaughtException') {
+								if (originalOnerrorHandler) {
+									global.onerror = originalOnerrorHandler;
+								} else {
+									global.onerror = function() {};
+								}
+								var i = uncaughtExceptionHandlers.indexOf(fn);
+								if (i !== -1) {
+									uncaughtExceptionHandlers.splice(i, 1);
+								}
+							}
 						};
 
-						// /* eslint no-unused-vars: off */
-						// /* eslint-env commonjs */
+						/**
+						 * Implements uncaughtException listener.
+						 */
 
-						// /**
-						//  * Shim process.stdout.
-						//  */
+						process.on = function(e, fn) {
+							if (e === 'uncaughtException') {
+								global.onerror = function(err, url, line) {
+									fn(new Error(err + ' (' + url + ':' + line + ')'));
+									return !mocha.allowUncaught;
+								};
+								uncaughtExceptionHandlers.push(fn);
+							}
+						};
 
-						// process.stdout = require('browser-stdout')({ level: false });
+						// The BDD UI is registered by default, but no UI will be functional in the
+						// browser without an explicit call to the overridden `mocha.ui` (see below).
+						// Ensure that this default UI does not expose its methods to the global scope.
+						mocha.suite.removeAllListeners('pre-require');
 
-						// var Mocha = require('./lib/mocha');
+						var immediateQueue = [];
+						var immediateTimeout;
 
-						// /**
-						//  * Create a Mocha instance.
-						//  *
-						//  * @return {undefined}
-						//  */
+						function timeslice() {
+							var immediateStart = new Date().getTime();
+							while (
+								immediateQueue.length &&
+								new Date().getTime() - immediateStart < 100
+							) {
+								immediateQueue.shift()();
+							}
+							if (immediateQueue.length) {
+								immediateTimeout = setTimeout(timeslice, 0);
+							} else {
+								immediateTimeout = null;
+							}
+						}
 
-						// var mocha = new Mocha({ reporter: 'html' });
+						/**
+						 * High-performance override of Runner.immediately.
+						 */
 
-						// /**
-						//  * Save timer references to avoid Sinon interfering (see GH-237).
-						//  */
+						Mocha.Runner.immediately = function(callback) {
+							immediateQueue.push(callback);
+							if (!immediateTimeout) {
+								immediateTimeout = setTimeout(timeslice, 0);
+							}
+						};
 
-						// var Date = global.Date;
-						// var setTimeout = global.setTimeout;
-						// var setInterval = global.setInterval;
-						// var clearTimeout = global.clearTimeout;
-						// var clearInterval = global.clearInterval;
+						/**
+						 * Function to allow assertion libraries to throw errors directly into mocha.
+						 * This is useful when running tests in a browser because window.onerror will
+						 * only receive the 'message' attribute of the Error.
+						 */
+						mocha.throwError = function(err) {
+							uncaughtExceptionHandlers.forEach(function(fn) {
+								fn(err);
+							});
+							throw err;
+						};
 
-						// var uncaughtExceptionHandlers = [];
+						/**
+						 * Override ui to ensure that the ui functions are initialized.
+						 * Normally this would happen in Mocha.prototype.loadFiles.
+						 */
 
-						// var originalOnerrorHandler = global.onerror;
+						mocha.ui = function(ui) {
+							Mocha.prototype.ui.call(this, ui);
+							this.suite.emit('pre-require', global, null, this);
+							return this;
+						};
 
-						// /**
-						//  * Remove uncaughtException listener.
-						//  * Revert to original onerror handler if previously defined.
-						//  */
+						/**
+						 * Setup mocha with the given setting options.
+						 */
 
-						// process.removeListener = function(e, fn) {
-						// 	if (e === 'uncaughtException') {
-						// 		if (originalOnerrorHandler) {
-						// 			global.onerror = originalOnerrorHandler;
-						// 		} else {
-						// 			global.onerror = function() {};
-						// 		}
-						// 		var i = uncaughtExceptionHandlers.indexOf(fn);
-						// 		if (i !== -1) {
-						// 			uncaughtExceptionHandlers.splice(i, 1);
-						// 		}
-						// 	}
-						// };
+						mocha.setup = function(opts) {
+							if (typeof opts === 'string') {
+								opts = { ui: opts };
+							}
+							for (var opt in opts) {
+								if (opts.hasOwnProperty(opt)) {
+									this[opt](opts[opt]);
+								}
+							}
+							return this;
+						};
 
-						// /**
-						//  * Implements uncaughtException listener.
-						//  */
+						/**
+						 * Run mocha, returning the Runner.
+						 */
 
-						// process.on = function(e, fn) {
-						// 	if (e === 'uncaughtException') {
-						// 		global.onerror = function(err, url, line) {
-						// 			fn(new Error(err + ' (' + url + ':' + line + ')'));
-						// 			return !mocha.allowUncaught;
-						// 		};
-						// 		uncaughtExceptionHandlers.push(fn);
-						// 	}
-						// };
+						mocha.run = function(fn) {
+							var options = mocha.options;
+							mocha.globals('location');
 
-						// // The BDD UI is registered by default, but no UI will be functional in the
-						// // browser without an explicit call to the overridden `mocha.ui` (see below).
-						// // Ensure that this default UI does not expose its methods to the global scope.
-						// mocha.suite.removeAllListeners('pre-require');
+							var query = Mocha.utils.parseQuery(global.location.search || '');
+							if (query.grep) {
+								mocha.grep(query.grep);
+							}
+							if (query.fgrep) {
+								mocha.fgrep(query.fgrep);
+							}
+							if (query.invert) {
+								mocha.invert();
+							}
 
-						// var immediateQueue = [];
-						// var immediateTimeout;
+							return Mocha.prototype.run.call(mocha, function(err) {
+								// The DOM Document is not available in Web Workers.
+								var document = global.document;
+								if (
+									document &&
+									document.getElementById('mocha') &&
+									options.noHighlighting !== true
+								) {
+									Mocha.utils.highlightTags('code');
+								}
+								if (fn) {
+									fn(err);
+								}
+							});
+						};
 
-						// function timeslice() {
-						// 	var immediateStart = new Date().getTime();
-						// 	while (
-						// 		immediateQueue.length &&
-						// 		new Date().getTime() - immediateStart < 100
-						// 	) {
-						// 		immediateQueue.shift()();
-						// 	}
-						// 	if (immediateQueue.length) {
-						// 		immediateTimeout = setTimeout(timeslice, 0);
-						// 	} else {
-						// 		immediateTimeout = null;
-						// 	}
-						// }
+						/**
+						 * Expose the process shim.
+						 * https://github.com/mochajs/mocha/pull/916
+						 */
 
-						// /**
-						//  * High-performance override of Runner.immediately.
-						//  */
+						Mocha.process = process;
 
-						// Mocha.Runner.immediately = function(callback) {
-						// 	immediateQueue.push(callback);
-						// 	if (!immediateTimeout) {
-						// 		immediateTimeout = setTimeout(timeslice, 0);
-						// 	}
-						// };
+						/**
+						 * Expose mocha.
+						 */
 
-						// /**
-						//  * Function to allow assertion libraries to throw errors directly into mocha.
-						//  * This is useful when running tests in a browser because window.onerror will
-						//  * only receive the 'message' attribute of the Error.
-						//  */
-						// mocha.throwError = function(err) {
-						// 	uncaughtExceptionHandlers.forEach(function(fn) {
-						// 		fn(err);
-						// 	});
-						// 	throw err;
-						// };
+						global.Mocha = Mocha;
+						global.mocha = mocha;
 
-						// /**
-						//  * Override ui to ensure that the ui functions are initialized.
-						//  * Normally this would happen in Mocha.prototype.loadFiles.
-						//  */
+						// this allows test/acceptance/required-tokens.js to pass; thus,
+						// you can now do `const describe = require('mocha').describe` in a
+						// browser context (assuming browserification).  should fix #880
 
-						// mocha.ui = function(ui) {
-						// 	Mocha.prototype.ui.call(this, ui);
-						// 	this.suite.emit('pre-require', global, null, this);
-						// 	return this;
-						// };
-
-						// /**
-						//  * Setup mocha with the given setting options.
-						//  */
-
-						// mocha.setup = function(opts) {
-						// 	if (typeof opts === 'string') {
-						// 		opts = { ui: opts };
-						// 	}
-						// 	for (var opt in opts) {
-						// 		if (opts.hasOwnProperty(opt)) {
-						// 			this[opt](opts[opt]);
-						// 		}
-						// 	}
-						// 	return this;
-						// };
-
-						// /**
-						//  * Run mocha, returning the Runner.
-						//  */
-
-						// mocha.run = function(fn) {
-						// 	var options = mocha.options;
-						// 	mocha.globals('location');
-
-						// 	var query = Mocha.utils.parseQuery(global.location.search || '');
-						// 	if (query.grep) {
-						// 		mocha.grep(query.grep);
-						// 	}
-						// 	if (query.fgrep) {
-						// 		mocha.fgrep(query.fgrep);
-						// 	}
-						// 	if (query.invert) {
-						// 		mocha.invert();
-						// 	}
-
-						// 	return Mocha.prototype.run.call(mocha, function(err) {
-						// 		// The DOM Document is not available in Web Workers.
-						// 		var document = global.document;
-						// 		if (
-						// 			document &&
-						// 			document.getElementById('mocha') &&
-						// 			options.noHighlighting !== true
-						// 		) {
-						// 			Mocha.utils.highlightTags('code');
-						// 		}
-						// 		if (fn) {
-						// 			fn(err);
-						// 		}
-						// 	});
-						// };
-
-						// /**
-						//  * Expose the process shim.
-						//  * https://github.com/mochajs/mocha/pull/916
-						//  */
-
-						// Mocha.process = process;
-
-						// /**
-						//  * Expose mocha.
-						//  */
-
-						// global.Mocha = Mocha;
-						// global.mocha = mocha;
-
-						// // this allows test/acceptance/required-tokens.js to pass; thus,
-						// // you can now do `const describe = require('mocha').describe` in a
-						// // browser context (assuming browserification).  should fix #880
-
-						// var varName =
-						// 	typeof global !== 'undefined'
-						// 		? 'global'
-						// 		: typeof self !== 'undefined'
-						// 			? 'self'
-						// 			: typeof window !== 'undefined'
-						// 				? 'window'
-						// 				: '{}';
-						// throw new Error('Assigning mocha to global ' + varName);
 						module.exports = global;
 					}.call(
 						this,
@@ -10677,9 +10664,10 @@
 
 								/*istanbul ignore start*/ function _interopRequireDefault(obj) {
 									return obj && obj.__esModule ? obj : { default: obj };
-								} // Based on https://en.wikipedia.org/wiki/Latin_script_in_Unicode // // Ranges and exceptions: // Latin-1 Supplement, 0080–00FF //  - U+00D7  × Multiplication sign //  - U+00F7  ÷ Division sign // Latin Extended-A, 0100–017F // Latin Extended-B, 0180–024F
+								} // Based on https://en.wikipedia.org/wiki/Latin_script_in_Unicode // // Ranges and exceptions: // Latin-1 Supplement, 0080–00FF //  - U+00D7  × Multiplication sign //  - U+00F7  ÷ Division sign // Latin Extended-A, 0100–017F
 
-								/*istanbul ignore end*/ // IPA Extensions, 0250–02AF
+								/*istanbul ignore end*/ // Latin Extended-B, 0180–024F
+								// IPA Extensions, 0250–02AF
 								// Spacing Modifier Letters, 02B0–02FF
 								//  - U+02C7  ˇ &#711;  Caron
 								//  - U+02D8  ˘ &#728;  Breve
